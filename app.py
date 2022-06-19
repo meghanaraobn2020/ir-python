431f40bc998436ffc5dd9d0c95638677e9a2a12a
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2022-06-19 13:25:16 +0200

    Update app.py
    Removed unused code.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
-from python_scripts import RelevanceFeedback
+# from python_scripts import RelevanceFeedback
 
 app = Flask(__name__)
 CORS(app)
@@ -15,13 +15,6 @@ def hello_world():
 def get_docs_with_ranks():
     doc_names = ['document_1', 'document_2', 'document_3']
     return jsonify(doc_names)
-
-
-@app.route("/relevancefeedback")
-def get_docs_with_ranks():
-    doc_names = RelevanceFeedback()
-    return jsonify(doc_names)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
